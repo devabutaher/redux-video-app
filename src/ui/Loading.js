268e908c52aa0d8bd90f1492a50d8d30f8a6746d
Abu Taher <writerabutaher@gmail.com>
2024-01-29 00:21:18 +0600

    dynamic video grid

---
+++ src/ui/Loading.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-blue-400 col-span-12"></div>
+  );
+};
+
+export default Loading;
