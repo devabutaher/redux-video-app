2a7e8d6e539f6db72bfde34bd243fe996ead7775
Abu Taher <writerabutaher@gmail.com>
2024-01-29 18:18:35 +0600

    related videos slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+import relatedVideosSlice from "../features/relatedVideos/relatedVideosSlice";
 import tagsSlice from "../features/tags/tagsSlice";
 import videoSlice from "../features/video/videoSlice";
 import videosSlice from "../features/videos/videoSlice";
@@ -8,5 +9,6 @@ export const store = configureStore({
     videos: videosSlice,
     tags: tagsSlice,
     video: videoSlice,
+    relatedVideos: relatedVideosSlice,
   },
 });
